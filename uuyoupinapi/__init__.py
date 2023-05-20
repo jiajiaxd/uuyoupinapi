cfaf513849aed9d5acabb40bffac18efe1d79d06
甲甲 <i@jiajiaxd.com>
2023-05-20 20:54:53 +0800

    fix: 无法使用

--- uuyoupinapi/__init__.py
+++ uuyoupinapi/__init__.py
@@ -89,16 +89,6 @@ class UUAccount:
         else:
             raise Exception('Method not supported')
 
-    def get_steam_offer_id_by_order_id(self, order_id):
-        """
-        通过订单号获取steam交易号
-        :param order_id: 订单号
-        :return: steam交易报价号
-        """
-        return self.call_api('GET', '/api/trade/Order/OrderPagedDetail', data={
-            'orderNo': order_id
-        }).json()['Data']['SteamOfferId']
-
     def get_wait_deliver_list(self, game_id=730, return_offer_id=True):
         """
         获取待发货列表（出售）
@@ -106,20 +96,32 @@ class UUAccount:
         :param game_id: 游戏ID，默认为730(CSGO)
         :return: 待发货列表，格式为[{'order_id': '订单号', 'item_name': '物品名称', 'offer_id': 'steam交易报价号'}... , ...]
         """
-        data = self.call_api('POST', '/api/youpin/bff/trade/sell/page/v1/waitDeliver/waitDeliverList', data={
-            "gameId": game_id,
+        # data = self.call_api('POST', '/api/youpin/bff/trade/sell/page/v1/waitDeliver/waitDeliverList', data={
+        #     "gameId": game_id,
+        #     "pageIndex": 1,
+        #     "pageSize": 1000
+        # })
+        # wait_deliver_list = data.json()['data']['waitDeliverList']
+        # data_to_return = []
+        # if wait_deliver_list is not None:
+        #     for item in wait_deliver_list:
+        #         if item['orderInfoVO']['orderType'] == 1:
+        #             dict_to_append = dict()
+        #             dict_to_append['order_id'] = item['orderInfoVO']['orderNo']
+        #             dict_to_append['item_name'] = item['commodityInfoVO']['commodityName']
+        #             if return_offer_id:
+        #                 dict_to_append['offer_id'] = self.get_steam_offer_id_by_order_id(dict_to_append['order_id'])
+        #             data_to_return.append(dict_to_append)
+        data = self.call_api('POST', '/api/youpin/bff/trade/sale/v1/sell/list', data={
+            "keys": "",
+            "orderStatus": "140",
             "pageIndex": 1,
-            "pageSize": 1000
-        })
-        wait_deliver_list = data.json()['data']['waitDeliverList']
+            "pageSize": 100
+        }).json()['data']
         data_to_return = []
-        if wait_deliver_list is not None:
-            for item in wait_deliver_list:
-                if item['orderInfoVO']['orderType'] == 1:
-                    dict_to_append = dict()
-                    dict_to_append['order_id'] = item['orderInfoVO']['orderNo']
-                    dict_to_append['item_name'] = item['commodityInfoVO']['commodityName']
-                    if return_offer_id:
-                        dict_to_append['offer_id'] = self.get_steam_offer_id_by_order_id(dict_to_append['order_id'])
-                    data_to_return.append(dict_to_append)
+        for order in data['orderList']:
+            data_to_return.append({
+                'order_id': order['tradeOfferId'],
+                'item_name': order['productDetail']['commodityName'],
+            })
         return data_to_return
